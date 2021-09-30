a53e1b2adb726d47e2b9a8f2b2d95e8cc8dacd94
Gregory John Casamento <greg.casamento@gmail.com>
2021-09-30 01:42:42 -0400

    Initial Commit

---
+++ NSPopover_test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NSPopover_test
+//
+//  Created by Gregory Casamento on 9/30/21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
